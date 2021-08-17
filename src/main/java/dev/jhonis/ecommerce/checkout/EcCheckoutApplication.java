f8214fd76b1bc9ab24a7ff8791372b6398eace7a
Jhonis Santos dos Anjos <jhonis_10@hotmail.com.br>
2021-08-17 15:56:55 -0300

    Project created

---
+++ src/main/java/dev/jhonis/ecommerce/checkout/EcCheckoutApplication.java
@@ -0,0 +1,13 @@
+package dev.jhonis.ecommerce.checkout;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcCheckoutApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcCheckoutApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/jhonis/ecommerce/checkout/EcCheckoutApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.jhonis.ecommerce.checkout;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcCheckoutApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
